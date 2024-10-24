bf6f0664b5fb3276a730708b38ff77683f6fb70f
Caenis1 <carlossocial1479@gmail.com>
2024-10-24 20:50:18 -0300

    s

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int nota1 = 10;
+        int nota2 = 7;
+        int nota3 = 2;
+        int nota4 = 4;
+
+        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Esta é sua média:" + media);
+    }
+}
